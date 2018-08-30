24da0a3ca3dfc19e0545839c00bb9559ce65dbe4
Yanghwa <kellerj87@hotmail.com>
2018-08-30 13:51:08 -0400

    Dialogs and Toasts

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { switchMap } from 'rxjs/operators';
+import { Toasty } from 'nativescript-toasty';
 
 @Component({
     selector: 'app-dishdetail',
@@ -48,9 +49,10 @@ export class DishdetailComponent implements OnInit {
 
     addToFavorites() {
         if (!this.favorite) {
-          console.log('Adding to Favorites', this.dish.id);
-          this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-          
+            console.log('Adding to Favorites', this.dish.id);
+            this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+            const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+            toast.show();
         }
-      }
+    }
 }
