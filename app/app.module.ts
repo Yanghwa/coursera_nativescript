205130606f25174652649824d35680717d98aedb
Yanghwa <kellerj87@hotmail.com>
2018-09-14 11:40:32 -0400

    Modal Dialog

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,6 +17,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { ReservationModalComponent } from "./reservationmodal/reservationmodal.component";
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
@@ -58,8 +59,10 @@ import { baseURL } from './shared/baseurl';
         ContactComponent,
         AboutComponent,
         FavoritesComponent,
-        ReservationComponent
+        ReservationComponent,
+        ReservationModalComponent
     ],
+    entryComponents: [ReservationModalComponent],
     providers: [
         {provide: 'baseURL', useValue: baseURL},
         DishService,
