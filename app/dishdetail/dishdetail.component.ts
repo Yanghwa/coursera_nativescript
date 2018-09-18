6e6186063a7121cbc77ec6bda07e4012c1f6a2b0
Yanghwa <kellerj87@hotmail.com>
2018-09-18 12:30:21 -0400

    assignment2 with errors

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewContainerRef } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
@@ -9,6 +9,9 @@ import { Location } from '@angular/common';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Toasty } from 'nativescript-toasty';
+import { action } from "ui/dialogs";
+import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
+import { CommentComponent } from '../comment/comment.component';
 
 @Component({
     selector: 'app-dishdetail',
@@ -25,7 +28,8 @@ export class DishdetailComponent implements OnInit {
     favorite: boolean = false;
 
     constructor(private dishservice: DishService, private route: ActivatedRoute, private routerExtensions: RouterExtensions,
-        @Inject('baseURL') private baseURL, private favoriteservice: FavoriteService, private fonticon: TNSFontIconService,) {
+        @Inject('baseURL') private baseURL, private favoriteservice: FavoriteService, private fonticon: TNSFontIconService,
+        private modalService: ModalDialogService, private vcRef: ViewContainerRef) {
 
     }
 
@@ -53,6 +57,43 @@ export class DishdetailComponent implements OnInit {
             this.favorite = this.favoriteservice.addFavorite(this.dish.id);
             const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
             toast.show();
+        } else {
+            console.log('Deleting from Favorites', this.dish.id);
+            this.favorite = !this.favoriteservice.deleteFavorite(this.dish.id);
+            const toast = new Toasty("Deleted Dish "+ this.dish.id + " from favorites", "short", "bottom");
+            toast.show();
         }
+    }
+
+    createModalView() {
+        let options: ModalDialogOptions = {
+            viewContainerRef: this.vcRef,
+            fullscreen: false
+        };
+        this.modalService.showModal(CommentComponent, options)
+            .then((result: Comment) => {
+                console.log(result);
+                this.dish.comments.push(result);
+                this.numcomments = this.dish.comments.length;
+                let total = 0;
+                this.dish.comments.forEach((comment: Comment) => total += comment.rating);
+                this.avgstars = (total/this.numcomments).toFixed(2);
+            });
+    }
+
+    createAction() {
+        let options = {
+          title: "menu options",
+          cancelButtonText: "Cancel",
+          actions: ["Add/Delete a Favorite", "Add comment"]
+        };
+        action(options).then(result => {
+          if (result === "Add/Delete a Favorite") {
+            this.addToFavorites();
+          } else if (result === "Add comment") {  
+            console.log('Adding comment');
+            this.createModalView();
+          }
+        });
     }
 }
