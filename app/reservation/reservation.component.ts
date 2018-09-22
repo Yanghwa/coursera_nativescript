09a657beb24c89ba83d6b0e4cc1dda25d224fc48
Yanghwa <kellerj87@hotmail.com>
2018-09-22 14:55:57 -0400

    assignment3

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -4,8 +4,13 @@ import { Switch } from 'ui/switch';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
 import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
-import * as app from "application";
-import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Reservation } from '../shared/reservation';
+import { Page } from "ui/page";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { CouchbaseService } from '../services/couchbase.service';
+import * as enums from "ui/enums";
 
 @Component({
     selector: 'app-reservation',
@@ -16,15 +21,30 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 export class ReservationComponent implements OnInit {
 
     reservation: FormGroup;
+    formView: View;
+    feedbackView: View;
+    showFeedback: boolean = false;
+    newReservation: Reservation;
+    reservations: Array<Reservation>;
+    docId: string = "reservations";
 
     constructor(private formBuilder: FormBuilder, private modalService: ModalDialogService, 
-        private vcRef: ViewContainerRef) {
-
+        private vcRef: ViewContainerRef,private page: Page, private couchbaseService: CouchbaseService, 
+        private changeDetectorRef:ChangeDetectorRef) {
+            // this.changeDetectorRef.markForCheck();
             this.reservation = this.formBuilder.group({
                 guests: 3,
                 smoking: false,
                 dateTime: ['', Validators.required]
             });
+
+            this.reservations = [];
+            let doc = this.couchbaseService.getDocument(this.docId);
+            if (doc == null) {
+                this.couchbaseService.createDocument({"reservations": []}, this.docId);
+            } else {
+                this.reservations = doc.reservations;
+            }
     }
 
     ngOnInit() {
@@ -55,6 +75,14 @@ export class ReservationComponent implements OnInit {
 
     onSubmit() {
         console.log(JSON.stringify(this.reservation.value));
+        this.newReservation = this.reservation.value;
+        this.formView = this.page.getViewById<View>("formView");
+        this.feedbackView = this.page.getViewById<View>("feedbackView");
+
+        this.reservations.push(this.newReservation);
+        this.couchbaseService.updateDocument(this.docId, {"reservations": this.reservations});
+
+        this.animateSwitch();
     }
 
     createModalView(args) {
@@ -77,8 +105,26 @@ export class ReservationComponent implements OnInit {
 
     }
 
-    onDrawerButtonTap(): void {
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.showDrawer();
+    animateSwitch() {
+        this.formView.animate({
+            scale: {x: 0, y: 0},
+            opacity: 0,
+            duration: 500,
+            curve: enums.AnimationCurve.easeIn
+        }).then(() => {
+            this.feedbackView.animate({
+                scale: {x: 0, y: 0},
+                opacity: 0,
+                duration: 0,
+            }).then(() => {
+                this.showFeedback = true;
+                this.feedbackView.animate({
+                    scale: {x: 1, y: 1},
+                    opacity: 1,
+                    duration: 500,
+                    curve: enums.AnimationCurve.easeOut
+                });
+            });
+        });
     }
 }
