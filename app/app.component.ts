5e0efac3475a93ac19f8cce6e16e6b5ac5352369
Yanghwa <kellerj87@hotmail.com>
2018-09-22 21:29:03 -0400

    Platform and Connectivity

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import * as app from "application";
 import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition, SideDrawerLocation } from "nativescript-ui-sidedrawer";
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { filter } from "rxjs/operators";
+import { PlatformService } from './services/platform.service'; 
 // import { login, LoginResult } from "ui/dialogs";
 // import { getString, setString } from "application-settings";
 
@@ -13,57 +14,78 @@ import { filter } from "rxjs/operators";
     templateUrl: "app.component.html",
 })
 
-export class AppComponent implements OnInit { 
-    private _activatedUrl: string;
-    private _sideDrawerTransition: DrawerTransitionBase;
+export class AppComponent implements OnInit, OnDestroy { 
 
-    constructor(private router: Router, private routerExtensions: RouterExtensions, private fonticon: TNSFontIconService) {
+    constructor(private platformService: PlatformService) { } 
 
+    ngOnInit() { 
+        this.platformService.printPlatformInfo(); 
+        this.platformService.startMonitoringNetwork()
+            .subscribe((message: string) => {
+                console.log(message); 
+                
+            });
     }
 
-    ngOnInit(): void {
-        this._activatedUrl = "/menu";
-        this._sideDrawerTransition = new SlideInOnTopTransition();
+    ngOnDestroy() { 
 
-        this.router.events.pipe(filter((event:any) => event instanceof NavigationEnd))
-            .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
-    }
+        this.platformService.stopMonitoringNetwork(); 
 
-    get sideDrawerTransition(): DrawerTransitionBase {
-        return this._sideDrawerTransition;
-    }
+    } 
 
-    isComponentSelected(url: string): boolean {
-        return this._activatedUrl === url;
-    }
-
-    onNavItemTap(navItemRoute: string): void {
-        this.routerExtensions.navigate([navItemRoute], {
-            transition: {
-                name: "fade"
-            }
-        });
-
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.closeDrawer();
-    }
-
-    // displayLoginDialog() {
-    //     let options = {
-    //         title: "Login",
-    //         message: 'Type Your Login Credentials',
-    //         userName: getString("userName", ""),
-    //         password: getString("password",""),
-    //         okButtonText: "Login",
-    //         cancelButtonText: "Cancel"
-    //     }
-
-    //     login(options)
-    //         .then((loginResult: LoginResult) => {
-    //             setString("userName", loginResult.userName);
-    //             setString("password", loginResult.password);
-    //         },
-    //         () => { console.log('Login cancelled'); 
-    //     });
-    // }
 }
+
+// export class AppComponent implements OnInit { 
+//     private _activatedUrl: string;
+//     private _sideDrawerTransition: DrawerTransitionBase;
+
+//     constructor(private router: Router, private routerExtensions: RouterExtensions, private fonticon: TNSFontIconService) {
+
+//     }
+
+//     ngOnInit(): void {
+//         this._activatedUrl = "/menu";
+//         this._sideDrawerTransition = new SlideInOnTopTransition();
+
+//         this.router.events.pipe(filter((event:any) => event instanceof NavigationEnd))
+//             .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
+//     }
+
+//     get sideDrawerTransition(): DrawerTransitionBase {
+//         return this._sideDrawerTransition;
+//     }
+
+//     isComponentSelected(url: string): boolean {
+//         return this._activatedUrl === url;
+//     }
+
+//     onNavItemTap(navItemRoute: string): void {
+//         this.routerExtensions.navigate([navItemRoute], {
+//             transition: {
+//                 name: "fade"
+//             }
+//         });
+
+//         const sideDrawer = <RadSideDrawer>app.getRootView();
+//         sideDrawer.closeDrawer();
+//     }
+
+//     // displayLoginDialog() {
+//     //     let options = {
+//     //         title: "Login",
+//     //         message: 'Type Your Login Credentials',
+//     //         userName: getString("userName", ""),
+//     //         password: getString("password",""),
+//     //         okButtonText: "Login",
+//     //         cancelButtonText: "Cancel"
+//     //     }
+
+//     //     login(options)
+//     //         .then((loginResult: LoginResult) => {
+//     //             setString("userName", loginResult.userName);
+//     //             setString("password", loginResult.password);
+//     //         },
+//     //         () => { console.log('Login cancelled'); 
+//     //     });
+//     // }
+// }
--- app/app.module.ts
+++ app/app.module.ts
@@ -27,6 +27,7 @@ import { LeaderService } from './services/leader.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { FavoriteService } from './services/favorite.service';
 import { CouchbaseService } from './services/couchbase.service';
+import { PlatformService } from './services/platform.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -75,7 +76,8 @@ import { baseURL } from './shared/baseurl';
         LeaderService,
         ProcessHTTPMsgService,
         FavoriteService,
-        CouchbaseService
+        CouchbaseService,
+        PlatformService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
